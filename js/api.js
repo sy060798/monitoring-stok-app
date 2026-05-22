ae248e7449b899e0436030580d46c586c2e1eb3e
Gog12 <bejop0433@gmail.com>
2026-05-22 14:26:54 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbyztmqRUapHvNGZrEArbwT6ZO_CZ-xn4u4KF7OztOTv1llx8o3GnQWk9mhhKo0qcOUu/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzA76uSzpDu86HmQLn2WE4IL3uCB_iufWdac4rhe408z_GZmI4C3_y_EbZMqMHq6z0W/exec";
 
 async function request(payload) {
   const formData = new URLSearchParams();
