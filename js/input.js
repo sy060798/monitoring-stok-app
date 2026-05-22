518fefb24f4c6340504f7e4c3c937a0457fd8bf0
Gog12 <bejop0433@gmail.com>
2026-05-22 13:14:28 +0700

    Create input.js

---
+++ js/input.js
@@ -0,0 +1,47 @@
+const API_URL = "PASTE_URL_GOOGLE_APPS_SCRIPT_KAMU";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("btnSimpan");
+  if (btn) btn.addEventListener("click", tambahProduk);
+});
+
+async function tambahProduk() {
+  const nama = document.getElementById("nama").value;
+  const modal = Number(document.getElementById("modal").value);
+  const margin = Number(document.getElementById("margin").value);
+  const ppn = Number(document.getElementById("ppn").value);
+  const stok = Number(document.getElementById("stok").value);
+
+  const sku = generateSKU("BRG");
+  const harga_jual = hitungHargaJual(modal, margin, ppn);
+
+  const data = {
+    sku,
+    nama,
+    modal,
+    margin,
+    ppn,
+    harga_jual,
+    stok,
+    terjual: 0
+  };
+
+  await fetch(API_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      action: "add",
+      data: data
+    })
+  });
+
+  clearForm();
+}
+
+function clearForm() {
+  document.getElementById("nama").value = "";
+  document.getElementById("modal").value = "";
+  document.getElementById("margin").value = "";
+  document.getElementById("ppn").value = "";
+  document.getElementById("stok").value = "";
+}
