9ce9951312bb4606efffde85fd43038fe7620949
Gog12 <bejop0433@gmail.com>
2026-05-22 13:19:28 +0700

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+  setActiveMenu();
+});
+
 function toggleSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.classList.toggle("collapsed");
+}
+
+/* =========================
+   ACTIVE MENU
+========================= */
+function setActiveMenu() {
+  const links = document.querySelectorAll(".sidebar nav a");
+  const current = window.location.pathname.split("/").pop();
+
+  links.forEach(link => {
+    const href = link.getAttribute("href");
+
+    if (href === current) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
 }
