69ddcba9bbdaf0d07d2542ef3e1c5883d78a7d55
Gog12 <bejop0433@gmail.com>
2026-05-22 14:12:17 +0700

    Update input.js

--- js/input.js
+++ js/input.js
@@ -1,5 +1,3 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxS-Dy_OSDzMGVSpl6MbFCQ0aoyZJB17BG3Ue4sDJdCGm-8vOlPa6r8jV6akZ8fYiBQ/exec";
-
 document.addEventListener("DOMContentLoaded", () => {
   const btn = document.getElementById("btnSimpan");
   if (btn) btn.addEventListener("click", tambahProduk);
@@ -26,13 +24,9 @@ async function tambahProduk() {
     terjual: 0
   };
 
-  await fetch(API_URL, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      action: "add",
-      data: data
-    })
+  await request({
+    action: "add",
+    data: data
   });
 
   clearForm();
