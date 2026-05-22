fc75de9a74cad1ff4e8bfa1009ac870831e196d4
Gog12 <bejop0433@gmail.com>
2026-05-22 14:33:12 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,13 +1,20 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbzA76uSzpDu86HmQLn2WE4IL3uCB_iufWdac4rhe408z_GZmI4C3_y_EbZMqMHq6z0W/exec";
 
 async function request(payload) {
-  const formData = new URLSearchParams();
-  formData.append("data", JSON.stringify(payload));
+  try {
+    const formData = new URLSearchParams();
+    formData.append("data", JSON.stringify(payload));
 
-  const res = await fetch(API_URL, {
-    method: "POST",
-    body: formData
-  });
+    const res = await fetch(API_URL, {
+      method: "POST",
+      body: formData
+    });
 
-  return await res.json();
+    const json = await res.json();
+    return json;
+
+  } catch (err) {
+    console.error("API ERROR:", err);
+    return { error: true, message: err.toString() };
+  }
 }
